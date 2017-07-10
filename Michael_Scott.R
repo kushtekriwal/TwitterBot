2c36801fd3e151853c80d69982ae75533e69e391
Kush Tekriwal <kush.tekriwal@gmail.com>
2017-07-10 13:09:45 +0200

    Initial

--- Michael_Scott.R
+++ Michael_Scott.R
@@ -9,3 +9,5 @@ consumer_secret = "o0C0Oaf71HQT9VH73p90VwQgG6ELKJO3q6V66b8wzsmZteUDBm"
 access_token = "591002069-HpN0mLL4D8EKVC0BFtzhFLkC5OM93QRaAzsMs6rW"
 access_secret = "yBeIPg7CNLKGrDVVwZ2XzptOXfcPyDK4YuKcTloXZJgz5"
 setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
+
+tweets = searchTwitter("#michaelscott", n = 50, lang = "en")
