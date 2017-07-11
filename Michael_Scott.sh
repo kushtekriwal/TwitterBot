470014621ff807fa1ad7f10e0d9ab2d8dc274efe
Kush Tekriwal <kush.tekriwal@gmail.com>
2017-07-11 14:33:52 +0200

    update

--- Michael_Scott.sh
+++ Michael_Scott.sh
@@ -12,7 +12,7 @@ access_token = "884404333558804482-P4Y7f08sW7NVHi2eut0js1YuNYzmrdb"
 access_secret = "ARd9WkeAp7YFwYM1n6tUzPBeCTiepw8kUUcqZvXRaUG6Y"
 setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
 
-tweets = searchTwitter("#michaelscott", n = 73, lang = "en")
+tweets = searchTwitter("#michaelscott", n = 71, lang = "en")
 
 twitter.text = sapply(tweets, function(x) x$getText())
 
